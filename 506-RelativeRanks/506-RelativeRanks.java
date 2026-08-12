d1b7851e35f6ba6ee2715246af3edc904bae7d5a
Kaushall Dev <githeshkaushall@gmail.com>
2026-08-12 12:17:06 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.java
@@ -0,0 +1,33 @@
+// Last updated: 8/12/2026, 12:17:05 PM
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+
+        int[] sorted = score.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int rank = n - i;
+
+            if (rank == 1) {
+                map.put(sorted[i], "Gold Medal");
+            } else if (rank == 2) {
+                map.put(sorted[i], "Silver Medal");
+            } else if (rank == 3) {
+                map.put(sorted[i], "Bronze Medal");
+            } else {
+                map.put(sorted[i], String.valueOf(rank));
+            }
+        }
+
+        String[] ans = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = map.get(score[i]);
+        }
+
+        return ans;
+    }
+}
