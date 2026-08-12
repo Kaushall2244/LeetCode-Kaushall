271eb32e3c59a493cc0cf18f0495bbe916e2ba93
Kaushall Dev <githeshkaushall@gmail.com>
2026-08-12 12:07:07 +0530

    Create 1349-CheckIfItIsAStraightLine

---
+++ 1349-CheckIfItIsAStraightLine/1349-CheckIfItIsAStraightLine.java
@@ -0,0 +1,14 @@
+// Last updated: 8/12/2026, 12:07:07 PM
+class Solution {
+    public boolean checkStraightLine(int[][] c) {
+        int x0 = c[0][0], y0 = c[0][1];
+        int x1 = c[1][0], y1 = c[1][1];
+
+        for (int i = 2; i < c.length; i++) {
+            if ((long)(c[i][1] - y0) * (x1 - x0) !=
+                (long)(y1 - y0) * (c[i][0] - x0))
+                return false;
+        }
+        return true;
+    }
+}
